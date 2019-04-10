722720e8e5cb634922fffa3b8fb79360972d353d
Tyypos <tyyposofficial@gmail.com>
2019-04-10 06:35:16 -0700

    jared: setting everything up

---
+++ src/AssignmentSix.java
@@ -0,0 +1,15 @@
+/**
+ * This application... 
+ * 
+ * @author Team 6: Jared Cheney, Andrew Meraz, Chul Kim and Agustin Garcia
+ *
+ */
+public class AssignmentSix
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("Week 6! Wooooooo!");
+   }
+
+}
