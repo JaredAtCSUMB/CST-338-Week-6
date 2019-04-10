b55d630cf2558b9935b2ed59f0c24ed7a0b6af50
Tyypos <tyyposofficial@gmail.com>
2019-04-10 07:47:58 -0700

    jared: timer V1

--- src/AssignmentSix.java
+++ src/AssignmentSix.java
@@ -9,7 +9,45 @@ public class AssignmentSix
 
    public static void main(String[] args)
    {
-      System.out.println("Week 6! Wooooooo!");
+      Timer timer = new Timer();
+      timer.start();
    }
 
+}
+
+// TODO: need to handle pause and resume
+// Thread has wait() and notify()
+class Timer extends Thread
+{
+   private int timer;
+   
+   public Timer() {
+      timer = 0;
+   }
+     
+   @Override
+   public void run() {
+      while(timer < 11){
+         try {
+             doNothing();
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+         System.out.println("Timer: " + timer);
+         increment();
+      }
+      System.out.println("Timer ended");
+   }
+   
+   private synchronized void increment() {
+      timer++;
+   }
+   
+   private synchronized void doNothing() throws InterruptedException {
+      sleep(1000);
+   }
+   
+   public int getTimerValue() {
+      return timer;
+   }
 }
